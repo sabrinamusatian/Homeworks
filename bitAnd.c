4795b2d66351aed374f7fcc54296b897dcc072ba
sabrinamusatian <sabrina.musatian@yandex.ru>
2015-10-13 23:25:56 +0300

    Update bitAnd.c

--- bitAnd.c
+++ bitAnd.c
@@ -2,15 +2,15 @@
 
 int bitAnd(int x, int y)
 {
-    int cur = ~((~x)|(~y));
+    int cur = ~((~x) | (~y));
     return cur;
 }
 
 int main()
 {
-    int x,y;
+    int x, y;
     printf("Print x and y for bitAnd\n");
     scanf("%d %d", &x, &y);
-    printf("bitAnd: %d\n",bitAnd(x,y));
+    printf("bitAnd: %d\n", bitAnd(x, y));
     return 0;
 }
