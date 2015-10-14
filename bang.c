e3ad8689aea951f279596a8386a8abdedd90b93a
sabrinamusatian <sabrina.musatian@yandex.ru>
2015-10-14 23:32:46 +0300

    Create bang.c

---
+++ bang.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int bang(int x)
+{
+	int negativeX = ~x + 1;
+  int temprorary = ((x >> 31) & 1) | ((negativeX >> 31) & 1);
+	return temprorary ^ 1;
+}
+int main()
+{
+    int x;
+    printf("Print x\n");
+    scanf("%d", &x);
+    printf("bang is %d", bang(x));
+    return 0;
+}
