5bd9d58365a603353c334ab755f980249021619d
sabrinamusatian <sabrina.musatian@yandex.ru>
2015-11-05 22:50:40 +0300

    Update logicalShift.c

--- logicalShift.c
+++ logicalShift.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-int logicalShift (int x, int n)
+int logicalShift(int x, int n)
 {
-    int current = (~(1 << 31)) >> n;
-    x = x >> n;
-    return x & current;
+	int temp = 1;
+	x = x >> n;
+	temp = (((~(temp << 31)) >> n) << 1) + 1;
+	temp = temp & x;
+	return(temp);
 }
 
 int main()
