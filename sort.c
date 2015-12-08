840d7d737f855a4a9368856d192eddbb65cfe870
sabrinamusatian <sabrina.musatian@yandex.ru>
2015-12-08 23:32:22 +0300

    Update sort.c

--- sort.c
+++ sort.c
@@ -1,165 +1,190 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
-
-void bubble_sort (int *x, int n) //n^2
+typedef struct node 
 {
-    int i, j;
-    for (i = 0; i < n; i++)
+	int field;
+	struct node *next;
+} node;
+
+
+
+node* getLast(node *head)
+{
+    if (head == NULL)
     {
-        for (j = 0; j < n - 1; j++)
+        return NULL;
+    }
+    while (head->next)
+    {
+        head = head->next;
+    }
+    return head;
+}
+
+void add(node **head, int value)
+{
+    node *temp = (node*) malloc(sizeof(node));
+    if (temp == NULL)
+    {
+        printf("malloc failed in add\n");
+        return;
+    }
+    temp->field = value;
+    if (*head == NULL)
+    {
+		temp->next = *head;
+		*head = temp;
+		printf("The element has been added\n");
+        return;
+    }
+    else
+    {
+        node *last = getLast(*head);
+        temp->next = NULL;
+        last->next = temp;
+        printf("The element has been added\n");
+    }
+    return;
+}
+
+void rem(node **head, int value)
+{
+    node *temp = *head;
+    node *previous = NULL;
+    int check = 0;
+    while (temp != NULL)
+    {
+        if (temp->field == value)
         {
-            int temp;
-            if (x[j + 1] < x[j])
+            check = 1;
+            if (previous != NULL)
             {
-                int temp = x[j];
-                x[j] = x[j + 1];
-                x[j + 1] = temp;
+                previous->next = temp->next;
             }
+            else
+            {
+                *head = temp->next;
+            }
+            temp = NULL;
         }
+        else
+        {
+            previous = temp;
+            temp = temp->next;
+        }
+    }
+    if (check == 1)
+    {
+        printf("Element has been removed\n");
+    }
+    else
+    {
+        printf("Element has not been found in the list\n");
+    }
+    return;
+}
+
+void print(node *head)
+{
+    if (head == NULL)
+    {
+		printf("List is empty\n");
+		return;
+	}
+
+    while (head != NULL)
+    {
+        printf("%d ",head->field);
+        head = head->next;
     }
     printf("\n");
     return;
 }
 
-void  array_sort (int *x, int n) // n
+void clear(node *head)
 {
-    int i, j = 0;
-    int maxim = 100; // max value of variable in our array
-
-    int *temp = (int*) malloc(maxim * sizeof(int));
-
-    if (temp == NULL)
+    node *temp;
+    while (head != NULL)
     {
-        printf("malloc failed in array_sort\n");
-        return;
-    }
-
-    for (i = 0; i < 100; i++)
-    {
-        temp[i] = 0;
-    }
-    for (i = 0; i < n; i++)
-    {
-        temp[x[i]]++;
-    }
-    i = 0;
-    while (j < n)
-    {
-        while (temp[i] > 0)
-        {
-            x[j] = i;
-            temp[i]--;
-            j++;
-        }
-        i++;
-    }
-    free(temp);
-    return;
-}
-
-// The whole piece just for the merge sort with all helper functions
-
-// Helper function for finding the max of two numbers
-int max(int x, int y)
-{
-    if(x > y)
-    {
-        return x;
-    }
-    else
-    {
-        return y;
+        temp = head;
+        head =(head)->next;
+        free(temp);
     }
 }
 
-void merge_helper(int *input, int left, int right, int *temp)
+void createRing(node **head)
 {
-    if(right == left + 1)
+    if (*head == NULL)
     {
+        printf("The list is empty. It cannot be a ring\n");
         return;
     }
-    else
-    {
-        int i = 0;
-        int length = right - left;
-        int midpoint = length/2;
-        int l = left, r = left + midpoint;
-
-        //sort subarrays
-        merge_helper(input, left, left + midpoint, temp);
-        merge_helper(input, left + midpoint, right, temp);
-
-        // merge the arrays together
-        for(i = 0; i < length; i++)
-        {
-            if(l < left + midpoint && (r == right || max(input[l], input[r]) == input[r]))
-            {
-                temp[i] = input[l];
-                l++;
-            }
-            else
-            {
-                temp[i] = input[r];
-                r++;
-            }
-        }
-
-        for(i = left; i < right; i++)
-        {
-            input[i] = temp[i - left];
-        }
-    }
+    node *last = getLast(*head);
+    last->next = (*head);
 }
 
-int merge_sort(int *input, int size)
+void checkRing(node **head)
 {
-    int *temp = (int *)malloc(size * sizeof(int));
-    if (temp == NULL)
+	if (*head == NULL)
     {
-        printf("malloc failed in merge\n");
-        return;
-    }
-    merge_helper(input, 0, size, temp);
-    free(temp);
-    return;
+        printf("The list is empty. It cannot be a ring\n");
+		return;
+	}
+
+	node *temp1 = *head, *temp2 = (*head)->next;
+
+	while (temp2->next && temp2->next->next && temp1 != temp2)
+    {
+		temp2 = temp2->next->next;
+		temp1 = temp1->next;
+	}
+	if (temp1 == temp2)
+	{
+		printf("It is a ring\n");
+		return;
+	}
+	printf("It is not a ring\n");
+	return;
 }
 
 int main()
 {
-    int i, n = 100000000;
-    int *x = (int*)malloc(n * sizeof(int));
-    if (x == NULL)
+    node *head = NULL;
+    int val, check = 0;
+	char c;
+	while (check == 0)
     {
-        printf("malloc failed in main\n");
-        return;
-    }
+		scanf("%c", &c);
+		switch(c)
+		{
+			case 'a':
+				scanf("%d", &val);
+				add(&head, val);
+			break;
 
-    srand(time(NULL));
-    for (i = 0; i < n; i++)
-    {
-        x[i] = rand() % 100; // generate numbers between 0 and 99
-    }
+			case 'r':
+				scanf("%d", &val);
+				rem(&head, val);
+			break;
 
-    clock_t start, result;
-    start = clock();
-    merge_sort(x,n);
-    result = clock() - start;
+			case 'p':
+				print(head);
+			break;
 
-    printf("time: %.4f", (double)result/CLOCKS_PER_SEC);
+			case 's':
+                createRing(&head);
+            break;
 
-    free(x);
+            case 'c':
+                checkRing(&head);
+            break;
+
+			case 'q':
+				clear(head);
+				check = 3;
+			break;
+		}
+	}
     return 0;
 }
-
-/*	size     n       n^2     n*logn
-    5       0.0000	0.0000	 0.00000
-	10  	0.0000 	0.0000 	 0.0000
-	100 	0.0000	0.0000	 0.0000
-	1k  	0.0000 	0.0150   0.0000
-	10k 	0.0000	0.5930	 0.0000
-	100k	0.0000 	50.8560  0.0470
-	1M  	0.0160 	n/a  	 0.4050
-	10M 	0.0780	n/a      4.0400
-    100M	0.6400	n/a      40.2230 */
